9ed368afe256429e8de887c7bd6bc9c7b23997a2
DavideC03 <help@castellanidavide.it>
2021-07-11 14:40:11 +0200

    Auto-update

--- bin/lca.cpp
+++ bin/lca.cpp
@@ -1,42 +1,67 @@
-/**
- * @file lca.cpp
- *
- * @version 01.01 2021519
- *
- * @brief https://training.olinfo.it/#/task/lca/statement
- *
- * @ingroup lca
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
- */
-
-// Includes
 #include <bits/stdc++.h>
 using namespace std;
 
-// Variabiles
-int N;
+vector <vector <pair <int, int> > > collegamenti;
+int gN;
+queue <int> q;
+vector <vector <int> > used;
 
-// Main code
-int main()
+void inizia(int N, int T, int A[], int B[], int C[])
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    collegamenti.resize(N);
+    gN = N;
+    used.resize(N);
 
-  // Input
-  cin >> N;
+    for (size_t i = 0; i < N - 1; ++i)
+    {
+        collegamenti[A[i]].push_back(make_pair(B[i], C[i]));
+        collegamenti[B[i]].push_back(make_pair(A[i], C[i]));
+    }
 
-  // Code
-  // ...
+    for (size_t i = 0; i < N; ++i)
+    {
+        used[i].resize(N, -1);
+        q.push(i);
+        used[i][i] = 0;
 
-  // Output
-  cout << N << endl;
+        while (!q.empty())
+        {
+            int c = q.front();
+            q.pop();
 
-  // End
-  return 0;
+            for (size_t j = 0; j < collegamenti[c].size(); ++j)
+            {
+                if(used[i][collegamenti[c][j].first] == -1)
+                {
+                    used[i][collegamenti[c][j].first] = (T == 0 ? used[i][c] + 1:
+                                                         (T == 1 ? used[i][c] + collegamenti[c][j].second:
+                                                          (T == 2 ? min(used[i][c], collegamenti[c][j].second):
+                                                           (T == 3 ? used[i][c] + (collegamenti[c][j].second > 0 ? collegamenti[c][j].second : 0):
+                                                            -1))));
+                    q.push(collegamenti[c][j].first);
+                }
+            }
+        }
+    }
 }
+
+long long int distanza1(int u, int v)
+{
+    return used[u][v];
+}
+
+long long int distanza2(int u, int v)
+{
+    return used[u][v];
+}
+
+long long int minimo(int u, int v)
+{
+    return used[u][v];
+}
+
+long long int massimo(int u, int v)
+{
+    return used[u][v];
+}
+
